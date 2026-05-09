c45c403cd73fcaf05db8d596c1d5d175e3a9b3d5
Jin409 <sualng123@gmail.com>
2026-05-09 20:55:43 +0900

    refactor: 키 패턴 상수로 분리

--- src/main/java/com/hotdeal/reservation/queue/QueueKeys.java
+++ src/main/java/com/hotdeal/reservation/queue/QueueKeys.java
@@ -2,11 +2,14 @@ package com.hotdeal.reservation.queue;
 
 public class QueueKeys {
 
+    private static final String QUEUE_PREFIX = "queue:product:";
+    private static final String ENTERED_PREFIX = "queue:entered:";
+
     public static String queue(Long productId) {
-        return "queue:product:" + productId;
+        return QUEUE_PREFIX + productId;
     }
 
     public static String entered(Long productId) {
-        return "queue:entered:" + productId;
+        return ENTERED_PREFIX + productId;
     }
 }
