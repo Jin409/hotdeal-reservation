33ea05e20b7448c822045be642372288cb4bef5c
Jin409 <sualng123@gmail.com>
2026-05-11 22:28:33 +0900

    feat: 재시도 로직 추가

--- src/main/java/com/hotdeal/reservation/payment/processor/CreditCardProcessor.java
+++ src/main/java/com/hotdeal/reservation/payment/processor/CreditCardProcessor.java
@@ -6,6 +6,8 @@ import com.hotdeal.reservation.payment.client.PgClient;
 import com.hotdeal.reservation.payment.client.PgException;
 import com.hotdeal.reservation.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +17,11 @@ public class CreditCardProcessor implements PaymentProcessor {
     private final PgClient pgClient;
 
     @Override
+    @Retryable(
+            retryFor = PgException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     public void process(String idempotencyKey, User user, long amount) {
         try {
             pgClient.charge(idempotencyKey, amount);
